316100fbbe37f6c6fa8f8f236efc3d77cbda6710
Casey Cooper <caseykcooper14@gmail.com>
2021-04-11 15:11:36 -0700

    Import pandas/numpy and initialize data file

--- mlb-quasi-wins-shares.py
+++ mlb-quasi-wins-shares.py
@@ -1,5 +1,10 @@
-library (readr)
+import numpy as np
+import pandas as pd
+
+from pandas import Series, DataFrame
 
 urlfile="https://raw.githubusercontent.com/fivethirtyeight/data/master/mlb-quasi-win-shares/quasi_winshares.csv"
 
-mydata<-read_csv(url(urlfile))
+mydata = pd.read_csv(urlfile)
+print(mydata.head(5))
+
