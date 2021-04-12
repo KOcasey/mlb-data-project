853b9fafd782b8767282cc50ea3c0022711ba79e
Casey Cooper <caseykcooper14@gmail.com>
2021-04-11 21:38:24 -0700

    Add trenline and labels to BWPS graph

--- mlb-quasi-wins-shares.py
+++ mlb-quasi-wins-shares.py
@@ -8,10 +8,24 @@ urlfile="https://raw.githubusercontent.com/fivethirtyeight/data/master/mlb-quasi
 
 mydata = pd.read_csv(urlfile)
 
-# bestWarPerSeason = mydata[['name_common','team_ID', 'year_ID', 'WAR162']]
-# bestWarPerSeason = bestWarPerSeason.groupby('year_ID')
-# bestWarPerSeason = bestWarPerSeason.max('WAR162')
 bestWarPerSeason = mydata.groupby('year_ID', as_index=False)['WAR162'].max()
 bestWarPerSeason = pd.merge(bestWarPerSeason, mydata, on='WAR162')
-print(bestWarPerSeason.tail(5))
+bwps_x = bestWarPerSeason['year_ID_x']
+bwps_y = bestWarPerSeason['WAR162']
+plt.scatter(bwps_x, bwps_y, s=10)
+
+# Add trendline
+z = np.polyfit(bwps_x, bwps_y, 1)
+p = np.poly1d(z)
+plt.plot(bwps_x,p(bwps_x),"r--")
+
+#add title
+plt.title('Highest WAR of Each MLB Season Since 1901')
+
+#add x and y labels
+plt.xlabel('Year')
+plt.ylabel('WAR (wins above replacement)')
+
+plt.show()
+# print(bestWarPerSeason.columns)
 
