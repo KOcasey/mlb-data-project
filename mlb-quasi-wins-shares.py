f5b2e7dffd17daa88b79a6152ca6b0342ed78b0b
Casey Cooper <caseykcooper14@gmail.com>
2021-04-11 21:03:18 -0700

    Create df for bestWarPerSeason visualization

--- mlb-quasi-wins-shares.py
+++ mlb-quasi-wins-shares.py
@@ -1,10 +1,17 @@
 import numpy as np
 import pandas as pd
-
 from pandas import Series, DataFrame
+
+import matplotlib.pyplot as plt
 
 urlfile="https://raw.githubusercontent.com/fivethirtyeight/data/master/mlb-quasi-win-shares/quasi_winshares.csv"
 
 mydata = pd.read_csv(urlfile)
-print(mydata.head(5))
+
+# bestWarPerSeason = mydata[['name_common','team_ID', 'year_ID', 'WAR162']]
+# bestWarPerSeason = bestWarPerSeason.groupby('year_ID')
+# bestWarPerSeason = bestWarPerSeason.max('WAR162')
+bestWarPerSeason = mydata.groupby('year_ID', as_index=False)['WAR162'].max()
+bestWarPerSeason = pd.merge(bestWarPerSeason, mydata, on='WAR162')
+print(bestWarPerSeason.tail(5))
 
