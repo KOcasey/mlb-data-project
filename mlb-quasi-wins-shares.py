0ad24a1e1909699bf9d297e9758623e629907c1e
Casey Cooper <caseykcooper14@gmail.com>
2021-04-23 16:03:23 -0700

    mistake

--- mlb-quasi-wins-shares.py
+++ mlb-quasi-wins-shares.py
@@ -32,14 +32,14 @@ mydata = pd.read_csv(urlfile)
 
 #warPerPos plots data wrangling
 war2019 = mydata.loc[mydata['year_ID'] == 2019]
-war2019SS = war2019.loc[war2019['def_pos'].str.contains('.*SS.*')==True]
-war20192B = war2019.loc[war2019['def_pos'].str.contains('.*2B.*')==True]
-war20191B = war2019.loc[war2019['def_pos'].str.contains('.*1B.*')==True]
-war20193B = war2019.loc[war2019['def_pos'].str.contains('.*3B.*')==True]
-war2019LF = war2019.loc[war2019['def_pos'].str.contains('.*LF.*')==True]
-war2019CF = war2019.loc[war2019['def_pos'].str.contains('.*CF.*')==True]
-war2019RF = war2019.loc[war2019['def_pos'].str.contains('.*RF.*')==True]
-war2019C = war2019.loc[(war2019['def_pos'].str.contains('.*C.*')==True) & (war2019['def_pos'].str.contains('.*CF.*')==False)]
+war2019SS = war2019.loc[war2019['def_pos'].str.contains('SS.*')==True]
+war20192B = war2019.loc[war2019['def_pos'].str.contains('2B.*')==True]
+war20191B = war2019.loc[war2019['def_pos'].str.contains('1B.*')==True]
+war20193B = war2019.loc[war2019['def_pos'].str.contains('3B.*')==True]
+war2019LF = war2019.loc[war2019['def_pos'].str.contains('LF.*')==True]
+war2019CF = war2019.loc[war2019['def_pos'].str.contains('CF.*')==True]
+war2019RF = war2019.loc[war2019['def_pos'].str.contains('RF.*')==True]
+war2019C = war2019.loc[(war2019['def_pos'].str.contains('C.*')==True) & (war2019['def_pos'].str.contains('.*CF.*')==False)]
 
 #Set axis font size
 plt.rcParams['axes.labelsize'] = 10
@@ -50,40 +50,55 @@ fig, axs = plt.subplots(2, 4, sharey=True)
 #Format size and layout of subplots
 fig.tight_layout(rect=[0, 0.03, 1, 0.9], pad=0.4, w_pad=0.5, h_pad=5.0)
 
-#Title the figure
+#Title the warPerPos figure
 fig.suptitle('War Per Position 2019', fontsize=14, fontweight='bold')
 
 plt.sca(axs[0,0])
 plt.hist(war20192B['WAR162'], bins=20)
 axs[0,0].set(title='WAR for 2B 2019', xlabel='WAR(Wins Above Replacement)', ylabel='Count')
+axs[0,0].axvline(x=war20192B['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
 
 plt.sca(axs[0,1])
 plt.hist(war2019SS['WAR162'], bins=20)
 axs[0,1].set(title='WAR for SS 2019', xlabel='WAR(Wins Above Replacement)', ylabel='')
+axs[0,1].axvline(x=war2019SS['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
+
 
 plt.sca(axs[0,2])
 plt.hist(war20191B['WAR162'], bins=20)
 axs[0,2].set(title='WAR for 1B 2019', xlabel='WAR(Wins Above Replacement)', ylabel='')
+axs[0,2].axvline(x=war20191B['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
+
 
 plt.sca(axs[0,3])
 plt.hist(war20193B['WAR162'], bins=20)
 axs[0,3].set(title='WAR for 3B 2019', xlabel='WAR(Wins Above Replacement)', ylabel='')
+axs[0,3].axvline(x=war20193B['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
+
 
 plt.sca(axs[1,0])
 plt.hist(war2019LF['WAR162'], bins=20)
 axs[1,0].set(title='WAR for LF 2019', xlabel='WAR(Wins Above Replacement)', ylabel='Count')
+axs[1,0].axvline(x=war2019LF['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
+
 
 plt.sca(axs[1,1])
 plt.hist(war2019CF['WAR162'], bins=20)
 axs[1,1].set(title='WAR for CF 2019', xlabel='WAR(Wins Above Replacement)', ylabel='')
+axs[1,1].axvline(x=war2019CF['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
+
 
 plt.sca(axs[1,2])
 plt.hist(war2019RF['WAR162'], bins=20)
 axs[1,2].set(title='WAR for RF 2019', xlabel='WAR(Wins Above Replacement)', ylabel='')
+axs[1,2].axvline(x=war2019RF['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
+
 
 plt.sca(axs[1,3])
 plt.hist(war2019C['WAR162'], bins=20)
 axs[1,3].set(title='WAR for C 2019', xlabel='WAR(Wins Above Replacement)', ylabel='')
+axs[1,3].axvline(x=war2019C['WAR162'].mean(), color='r', linestyle='dashed', linewidth=2)
+
 plt.show()
 
 #print(warPerPos2019.loc[warPerPos2019['def_pos']].head(10))
